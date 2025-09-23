b30f2c0eb0ef6baf78db9303d05070fc603dd854
Shashank B S <shashank67676767@gmail.com>
2025-09-23 14:13:48 +0530

    new

---
+++ src/test/java/com/SBS/GamingClub/GamingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SBS.GamingClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
